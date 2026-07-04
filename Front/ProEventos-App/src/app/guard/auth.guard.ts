7911e0bae8738864adf00605aae535c3cfa5fbd0
douglasscdoug <douglasscdoug@gmail.com>
2026-07-03 21:48:37 -0300

    refactor: refatora o authGuard para melhorar o fluxo de autenticação do usuário e o tratamento de erros

--- Front/ProEventos-App/src/app/guard/auth.guard.ts
+++ Front/ProEventos-App/src/app/guard/auth.guard.ts
@@ -4,12 +4,34 @@ import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const taoster = inject(ToastrService)
+  const toastr = inject(ToastrService);
+  const user = localStorage.getItem('user');
 
-  if(localStorage.getItem('user') !== null)
-    return true;
+  if (!user) {
+    toastr.info('Usuário não autenticado!');
+    router.navigate(['/user/login']);
+    return false;
+  }
 
-  taoster.info('Usuário não autenticado!');
-  router.navigate(['/user/login']);
-  return false;
+  const token = JSON.parse(user).token;
+
+  if (!token) {
+    localStorage.removeItem('user');
+    toastr.warning('Sessão inválida!');
+    router.navigate(['/user/login']);
+    return false;
+  }
+
+  const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+  const expirationDate = tokenPayload.exp * 1000;
+  const isExpired = Date.now() > expirationDate;
+
+  if (isExpired) {
+    localStorage.removeItem('user');
+    toastr.warning('Sessão expirada. Faça login novamente.');
+    router.navigate(['/user/login']);
+    return false;
+  }
+
+  return true;
 };
